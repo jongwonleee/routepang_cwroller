9620bd7c0c3a00510cebff227d01327fafd4391f
안찬영 <an20171@naver.com>
2019-11-06 11:33:56 +0900

    로그인 추가 / 스크롤 방식 변경

--- routepang/controller/CrawlingController.py
+++ routepang/controller/CrawlingController.py
@@ -10,10 +10,23 @@ class CrawlingController:
     Driver_Dir = "/Users/cy/PycharmProjects/chromedriver"
 
     def __init__(self):
+        self.usrId = "an20171@naver.com"
+        self.pwd = "Adhlee00!@!@"
+
         self.driver = webdriver.Chrome(self.Driver_Dir)
         self.driver.implicitly_wait(10)
-        self.driver.get("https://www.instagram.com/explore/tags/sample/")
-        # 페이지가 인스타 그램인지 확
+
+        # ------- Instagram Login -------- #
+        self.driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
+        elem = self.driver.find_element_by_xpath("//input[@name='username']")
+        elem.send_keys(self.usrId)
+        elem = self.driver.find_element_by_xpath("//input[@name='password']")
+        elem.send_keys(self.pwd)
+        self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[4]/button").click()
+        time.sleep(2)
+        self.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[3]/button[2]").click()
+
+        # 페이지가 인스타 그램인지 확인
         assert "Instagram" in self.driver.title
 
     def __del__(self):
@@ -21,69 +34,66 @@ class CrawlingController:
 
     # 해당 명소에 대한 최신 게시물 url 리스트 return
     def getAllArticle(self, request):
+        time.sleep(3)
         t = time.time()
         print(t, 'start get all article:')
 
-        # db에 저장할 url의 리스트를 담아 놓을 리스
+        # db에 저장할 url의 리스트를 담아 놓을 리스트
         url_list = []
-
-        # base_url = "https://www.instagram.com"
-        # url = base_url + "/explore/tags/" + request
-        # self.driver.get(url)
 
         elem = self.driver.find_element_by_xpath("//input[@placeholder='검색']")
         # location_name으로 검색창에 검색
         # name에 태그에 들어가면 안되는 문자도 사용 가능
         elem.send_keys(request)
 
+        time.sleep(2)
         # 검색 결과 맨 위를 클릭
         try:
             self.driver.find_element_by_css_selector('div.fuqBx > a').click()
-        # 검색 결과가 없는 경
+        # 검색 결과가 없는 경우 체크
         except NoSuchElementException:
             return url_list
+        # 검색한 페이지가 로딩될 때 까지 대기
+        time.sleep(3)
 
-        #TODO 검색해서 들어갔는데 location이라서 게시물이 없는 경우
+        print(t, request, '-> start')
 
-        # body 태그를 태그 이름으로 찾기
-        # elem = self.driver.find_element_by_tag_name("body")
-        # pagedowns = 1
+        page = self.driver.find_element_by_tag_name("body")
+        pagedown = 1
+        # 스크롤 하면서 article url 크롤링
+        # 최대 50번 스크롤
+        while pagedown < 50:
 
-        last_height = self.driver.execute_script("return document.body.scrollHeight")
-
-        # pagedown수 만큼 스크롤
-        while True:
-            # for i in range(2):
-            #     elem.send_keys(Keys.PAGE_DOWN)
-
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            for i in range(4):
+                page.send_keys(Keys.PAGE_DOWN)
 
             print(time.time(), 'pressing pagedown with delay')
-            time.sleep(2)
+            time.sleep(1)
 
+            # Search by Tag
             link_url = self.driver.find_elements_by_css_selector('article.KC1QD > div > div > div > div > a')
+            # Search by Location
+            if not len(link_url) > 0:
+                link_url = self.driver.find_elements_by_css_selector('article.vY_QD > div > div > div > div > a')
+            # Search by Page
+            if not len(link_url) > 0:
+                link_url = self.driver.find_elements_by_css_selector('article.FyNDV > div > div > div > div > a')
 
             # 해당 게시물에 #{location} 태그가 있는지 확인(보류) #
 
             for i in link_url:
-                # DB에 들어있는 게시에서 중복 확인
+                # DB에 들어있는 article data 중복 확인
                 if not Article.objects.filter(url=i).exists():
                     url_list.append(i.get_attribute('href'))
                 #TODO 사진 카테고리 확인해서 목적에 맞지 않는 게시물인 경우
 
+            # 중복 url 제거
+            url_list = list(set(url_list))
             # 가져온 게시물이 50개 이상이면 break
-            if len(url_list) >= 50:
+            print('length of Url List:', len(url_list))
+            if len(url_list) >= 20:
                 break
-
-            # pagedowns += 1
-
-            new_height = self.driver.execute_script("return document.body.scrollHeight")
-            # 스크롤이 더 이상 내려가지 않으면 break
-            if new_height == last_height:
-                break
-            last_height = new_height
-
-        url_list = list(set(url_list))
+            pagedown += 1
 
         print(time.time(), 'elapsed time:', time.time() - t)
 
